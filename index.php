2a49cee00e1b0ae2c219fbd5bf2a5142994d986b
Nielsington <declercqniels@hotmail.com>
2023-03-21 13:28:22 +0100

    Add function to create new item

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ require_once 'classes/DucksRepository.php';
 $databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
 $databaseManager->connect();
 
-$cardRepository = new DucksRepository($databaseManager);
-$ducks = $cardRepository->get();
+$duckRepository = new DucksRepository($databaseManager);
+$ducks = $duckRepository->get();
 
 // If nothing is specified, it will remain empty (home should be loaded)
 $action = $_GET['action'] ?? null;
@@ -22,7 +22,7 @@ $action = $_GET['action'] ?? null;
 // Load the relevant action
 switch ($action) {
     case 'create':
-        create();
+        create($ducks, $duckRepository);
         break;
     default:
         overview($ducks);
@@ -34,7 +34,18 @@ function overview($ducks)
     require 'overview.php';
 }
 
-function create()
+function create($ducks, $duckRepository)
 {
     // TODO: provide the create logic
+
+    $price = $_GET['price'];
+    $rarity = $_GET['rarity'];
+    $color = $_GET['color'];
+    $theme = $_GET['theme'];
+    $manufacturer = $_GET['manufacturer'];
+
+    $duckRepository->create($price, $rarity, $color, $theme, $manufacturer);
+    require 'overview.php';
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit();
 }
